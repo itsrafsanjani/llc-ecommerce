a2d0b6cb1e3da2d55db2307f061b6dbcc5cf281d
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2020-12-14 17:11:52 +0600

    user-factory and user-table-seeder created

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,9 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
+            'phone_number' => $this->faker->unique()->phoneNumber,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'email_verified_at' => now(),
             'remember_token' => Str::random(10),
         ];
     }
