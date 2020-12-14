09c6ef21af3fe42900a28a463c17b9d5a0ad30b7
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2020-12-15 02:23:57 +0600

    initial css and js file configured

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.styles([
+        'resources/css/bootstrap.min.css',
+        'resources/css/app.css',
+    ], 'public/css/app.css')
+    .js([
+        'resources/js/app.js',
+        'resources/js/custom.js',
+    ], 'public/js/app.js')
+    .version();
